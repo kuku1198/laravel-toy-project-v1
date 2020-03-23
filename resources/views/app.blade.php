e3256870239365f94f09df785e044c68528685b0
어영규 <kuku1198@gmail.com>
2020-03-23 18:55:26 +0900

    vuetify css 제거

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,6 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
 
         <title>To Do list</title>
     </head>
