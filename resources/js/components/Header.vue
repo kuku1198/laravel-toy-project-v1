0a59d2b5f10f44e42a3491c546dce5dbb00d4f78
어영규 <kuku1198@gmail.com>
2020-03-25 01:03:26 +0900

    header nav component 추가

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,17 +1,24 @@
 <template>
+    <v-app>
+        <Navigation></Navigation>
 
-
-    <v-app-bar
-            app
-            clipped-left
-    >
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
-        <v-toolbar-title>To Do List</v-toolbar-title>
-    </v-app-bar>
+        <v-app-bar
+                app
+                clipped-left
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+            <v-toolbar-title>To Do List</v-toolbar-title>
+        </v-app-bar>
+    </v-app>
 </template>
 
 <script>
+    import Navigation from "./Navigation";
+
     export default {
+        components: {
+            Navigation
+        },
         data: () => ({
             drawer: null,
         })
