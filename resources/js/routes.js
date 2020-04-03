c2794246b51149a6fd855bb09188232297feb381
어영규 <kuku1198@gmail.com>
2020-04-03 23:01:00 +0900

    login routes add

---
+++ resources/js/routes.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Login from './components/Login';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/login',
+        component: Login,
+        name: 'Login',
+    },
+];
+
+const router = new VueRouter({
+    routes,
+});
+
+export default router;
