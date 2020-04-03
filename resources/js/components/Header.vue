c3f45a19ebb4a85a56521c832a28811771ec538a
어영규 <kuku1198@gmail.com>
2020-04-03 23:09:21 +0900

    prettier code reformat

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,28 +1,33 @@
 <template>
-    <v-app>
-        <Navigation v-bind:is-drawer=drawer></Navigation>
+    <div>
+        <navigation v-bind:is-drawer="drawer"></navigation>
 
-        <v-app-bar
-                app
-                clipped-left
-        >
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+        <v-app-bar app clipped-left>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
             <v-toolbar-title>To Do List</v-toolbar-title>
+            <v-spacer></v-spacer>
+
+            <v-btn icon to="/login">
+                <v-icon>mdi-login</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>mdi-account</v-icon>
+            </v-btn>
         </v-app-bar>
-    </v-app>
+    </div>
 </template>
 
 <script>
-    import Navigation from "./Navigation";
+import navigation from './Navigation';
 
-    export default {
-        components: {
-            Navigation
-        },
-        data: function () {
-            return {
-                drawer: false
-            }
-        }
-    }
+export default {
+    components: {
+        navigation,
+    },
+    data() {
+        return {
+            drawer: false,
+        };
+    },
+};
 </script>
--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,34 +1,30 @@
 <template>
-    <v-navigation-drawer
-            v-model="isDrawer"
-            app
-            clipped
-    >
+    <v-navigation-drawer v-model="isDrawer" app clipped>
         <v-list dense>
-            <v-list-item link>
-                <v-list-item-action>
-                    <v-icon>mdi-view-dashboard</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>1</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item link>
-                <v-list-item-action>
-                    <v-icon>mdi-settings</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>2</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
+            <!-- <v-list-item link>
+                 <v-list-item-action>
+                     <v-icon>mdi-view-dashboard</v-icon>
+                 </v-list-item-action>
+                 <v-list-item-content>
+                     <v-list-item-title>1</v-list-item-title>
+                 </v-list-item-content>
+             </v-list-item>
+             <v-list-item link>
+                 <v-list-item-action>
+                     <v-icon>mdi-settings</v-icon>
+                 </v-list-item-action>
+                 <v-list-item-content>
+                     <v-list-item-title>2</v-list-item-title>
+                 </v-list-item-content>
+             </v-list-item>-->
         </v-list>
     </v-navigation-drawer>
 </template>
 
 <script>
-    export default {
-        props: {
-            isDrawer: Boolean,
-        }
-    }
+export default {
+    props: {
+        isDrawer: Boolean,
+    },
+};
 </script>
