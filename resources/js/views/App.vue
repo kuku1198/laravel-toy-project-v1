a0cdeb741c7972ee3dd5976a139c8b1a27970875
어영규 <kuku1198@gmail.com>
2020-04-03 23:10:00 +0900

    main components add

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,20 +1,24 @@
 <template>
     <v-app>
         <Header></Header>
+        <Main></Main>
         <Footer></Footer>
     </v-app>
 </template>
 
 <script>
-    import Header from '../components/Header';
-    import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Main from '../components/Main';
+import Footer from '../components/Footer';
 
-    export default {
-        components: {
-            Header, Footer
-        },
-        created() {
-            this.$vuetify.theme.dark = true
-        },
-    };
+export default {
+    components: {
+        Header,
+        Main,
+        Footer,
+    },
+    created() {
+        this.$vuetify.theme.dark = true;
+    },
+};
 </script>
