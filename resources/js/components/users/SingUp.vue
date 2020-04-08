c17036bb6fb8136f10f16231d0a86ba762f64afa
어영규 <kuku1198@gmail.com>
2020-04-08 22:34:54 +0900

    Login, SingUp component add

---
+++ resources/js/components/users/SingUp.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+                <v-toolbar color="red" dark flat>
+                    <v-toolbar-title>Sing Up</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                    <v-form>
+                        <v-text-field label="Name" name="name" type="text" />
+                        <v-text-field label="Email" name="eamil" type="text" />
+                        <v-text-field
+                            id="password"
+                            label="Password"
+                            name="password"
+                            type="password"
+                        />
+                        <v-text-field
+                            id="password_confirmation"
+                            label="Password Confirmation"
+                            name="password_confirmation"
+                            type="password"
+                        />
+                    </v-form>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer />
+                    <v-btn color="red">sing up</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {};
+</script>
