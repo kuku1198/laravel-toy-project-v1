bd11f61408f08ddb4c368864cbf89d5bc1c5e306
어영규 <kuku1198@gmail.com>
2020-04-09 20:50:30 +0900

    router link path 변경

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -7,11 +7,13 @@
             <v-toolbar-title>To Do List</v-toolbar-title>
             <v-spacer></v-spacer>
 
-            <v-btn icon to="/login">
-                <v-icon>mdi-login</v-icon>
+            <v-btn icon to="/user/login">
+                <v-icon title>mdi-login</v-icon>
+                <!--                <v-icon>mdi-logout</v-icon>-->
             </v-btn>
-            <v-btn icon>
+            <v-btn icon to="/user/sing-up">
                 <v-icon>mdi-account</v-icon>
+                <!--                <v-icon>mdi-account-box</v-icon>-->
             </v-btn>
         </v-app-bar>
     </div>
--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,22 +1,14 @@
 <template>
     <v-navigation-drawer v-model="isDrawer" app clipped>
         <v-list dense>
-            <!-- <v-list-item link>
-                 <v-list-item-action>
-                     <v-icon>mdi-view-dashboard</v-icon>
-                 </v-list-item-action>
-                 <v-list-item-content>
-                     <v-list-item-title>1</v-list-item-title>
-                 </v-list-item-content>
-             </v-list-item>
-             <v-list-item link>
-                 <v-list-item-action>
-                     <v-icon>mdi-settings</v-icon>
-                 </v-list-item-action>
-                 <v-list-item-content>
-                     <v-list-item-title>2</v-list-item-title>
-                 </v-list-item-content>
-             </v-list-item>-->
+            <v-list-item link to="/boards">
+                <v-list-item-action>
+                    <v-icon>mdi-view-dashboard</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>Boards</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
         </v-list>
     </v-navigation-drawer>
 </template>
