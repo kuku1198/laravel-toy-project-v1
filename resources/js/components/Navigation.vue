d7929e6c941af6a2d40977f05139645e6f315d85
어영규 <kuku1198@gmail.com>
2020-04-12 21:09:11 +0900

    icon 변경

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,12 +1,12 @@
 <template>
     <v-navigation-drawer v-model="isDrawer" app clipped>
         <v-list dense>
-            <v-list-item link to="/boards">
+            <v-list-item link to="/">
                 <v-list-item-action>
-                    <v-icon>mdi-view-dashboard</v-icon>
+                    <v-icon>mdi-format-list-checks</v-icon>
                 </v-list-item-action>
                 <v-list-item-content>
-                    <v-list-item-title>Boards</v-list-item-title>
+                    <v-list-item-title>To Do List</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
         </v-list>
