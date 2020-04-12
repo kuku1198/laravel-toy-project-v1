2b6eb7d45fb0138939d563bb2c696eebf4016938
어영규 <kuku1198@gmail.com>
2020-04-12 21:10:53 +0900

    component 추가

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-content>
+        <v-container fluid fill-height>
+            <router-view></router-view>
+        </v-container>
+    </v-content>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ resources/js/components/ToDoList.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-row class="ma-auto" align="start" justify="center">
+        <v-col cols="12" md="8">
+            <v-card>
+                <v-toolbar color="deep-orange">
+                    <v-toolbar-title>To Do List</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                    <v-form>
+                        <v-text-field name="eamil" type="text" />
+                    </v-form>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer />
+                    <v-btn color="deep-orange" @click="setItem">Add</v-btn>
+                </v-card-actions>
+            </v-card>
+
+            <v-card style="border-top: 1px solid white;">
+                <v-list rounded>
+                    <v-list-item-group>
+                        <v-list-item v-for="item in items">
+                            <v-list-item-content>
+                                <v-list-item-title
+                                    v-text="item.text"
+                                ></v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-item-group>
+                </v-list>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        items: [
+            { text: 'Real-Time' },
+            { text: 'Audience' },
+            { text: 'Conversions' },
+        ],
+    }),
+    methods: {
+        setItem() {
+            this.items.push({ text: 'test' });
+        },
+    },
+};
+</script>
